e060de12438b18b6c5c5c2a37a8554998a650679
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-19 12:29:56 -0600

    created a genders filter

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,1 @@
-// sample angular code
-
-var app = angular.module('myApp', []);
-
-app.controller('myController', ['$scope', function($scope) {
-  $scope.greeting = "Hello World!";
-}]);
+angular.module('lowExpecApp', []);
---
+++ src/js/filters.js
@@ -0,0 +1,9 @@
+angular.module('chatApp')
+.filter('genders', function() {
+  return function (gender) {
+    if      (gender === 0) { return 'Sanguine'; }
+    else if (gender === 1) { return 'Choleric'; }
+    else if (gender === 2) { return 'Melancholic'; }
+    else if (gender === 3) { return 'Phlegmatic'; }
+  }
+})
