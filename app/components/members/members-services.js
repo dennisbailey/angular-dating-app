e93a7fc690d00b860a9484154dd7356f1d4a4d58
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-19 20:55:13 -0600

    pushing so Micheal can take a look

--- app/components/members/members-services.js
+++ app/components/members/members-services.js
@@ -9,14 +9,7 @@ function memberSvcs($http) {
    
    getAll : function () {
           
-     return $http({
-        url: 'https://galvanize-student-apis.herokuapp.com/gdating/members?limit=10',
-        method: 'GET'
-      })
-     
-     .then( function (result) { return result })
-     
-     .catch( function (error) { return error; })
+     return $http.get('https://galvanize-student-apis.herokuapp.com/gdating/members?limit=10');
 
    } 
     
