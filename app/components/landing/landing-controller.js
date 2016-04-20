8b06b598eba40452235ec6af8903e71ce064f557
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-20 17:37:22 -0600

    Got the testimonials to cycle

---
+++ app/components/landing/landing-controller.js
@@ -0,0 +1,56 @@
+angular
+  .module('temperamentsApp')
+  .controller('LandingCtrl', LandingCtrl);
+
+LandingCtrl.$inject = ['$timeout'];
+
+function LandingCtrl($timeout) {
+  var vm = this;
+  
+  vm.testimonials = [
+    'Simply the best',
+    'Just Do IT!',
+    'Think Different',
+    "I\'m Lovin' it",
+    'Because You\'re Worth It',
+    'Got Milk?',
+    'Melts in Your Mouth, Not in Your Hands',
+    'Tastes So Good, Cats Ask for It by Name',
+    'Ding! You are now free to move about the country'
+  ];
+  
+  vm.users = [
+    'Leslie Odom',
+    'Christopher Jackson',
+    'Rory O\'Malley',
+    'Phillipa Soo',
+    'Renee Elise Goldsberry',
+    'Daveed Diggs',
+    'Anthony Ramos',
+    'Howell Binkley',
+    'Jasmine Cephas Jones'
+  ];
+  
+  vm.count = 0;
+  vm.user = vm.users[vm.count];
+  vm.testimonial = vm.testimonials[vm.count];
+    
+  vm.changeitup = function () {
+    $timeout(function () {
+      console.log(vm.count, vm.user, vm.testimonial);
+      
+      if (vm.count < 8) { vm.count++; }
+                          
+      else { vm.count = 0; };
+             
+      vm.user = vm.users[vm.count]; 
+      vm.testimonial = vm.testimonials[vm.count];
+             
+      vm.changeitup();
+      
+    }, 1000);
+  };
+  
+  vm.changeitup();
+  
+}
