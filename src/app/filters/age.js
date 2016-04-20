1afc37b13dfaa8eb46b4278c01d3251d434731b5
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-19 20:54:22 -0600

    Added attribution for the inspiration for this filter

--- src/app/filters/age.js
+++ src/app/filters/age.js
@@ -1,17 +1,20 @@
+// *** Adapted from srph's angular-age-filter on github https://github.com/srph/angular-age-filter *** //
+// *** srph drew inspiration from a Stack Overflow Answer by pixelbits http://stackoverflow.com/questions/24883308/convert-birthday-to-age-in-angularjs *** //
+// *** angular-age-filter © 2014+, Kier Borromeo (srph). Released under the MIT License. *** //
+
 angular
   .module('temperamentsApp')
   .filter('age', age);
 
 function age() {
-  
+
   return function(input, current) {
-    current = Date.now();
-    
+
     // Difference in milliseconds
-    var ageDiffMs =  current - new Date(input).getTime();
-    var ageDate = new Date(ageDiffMs);
+    var ageDiff =  Date.now() - new Date(input).getTime();
+    var ageDate = new Date(ageDiff);
 
     return Math.abs( ageDate.getUTCFullYear() - 1970 );
-  }
+  };
 
 }
