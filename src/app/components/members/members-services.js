9bf645a11ed84115ff99874cb3c056f4afb2173b
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-20 15:32:37 -0600

    Added loading animations to the members page

--- src/app/components/members/members-services.js
+++ src/app/components/members/members-services.js
@@ -8,7 +8,7 @@ function memberSvcs($http) {
   
   return {  
     getAll : function () {
-      return $http.get('https://galvanize-student-apis.herokuapp.com/gdating/members?limit=100');
+      return $http.get('https://galvanize-student-apis.herokuapp.com/gdating/members');
     },
    
     getOne : function (id) {
