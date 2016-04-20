81de8dd418e5a02f3d706b5ade40ca796b73d4c2
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-19 21:28:59 -0600

    Ran everything through the linter

--- src/app/components/members/members-services.js
+++ src/app/components/members/members-services.js
@@ -1,18 +1,17 @@
 angular
   .module('temperamentsApp')
   .service('memberSvcs', memberSvcs);
-  
-memberSvcs.$inject = ['$http']
-  
+
+memberSvcs.$inject = ['$http'];
+
 function memberSvcs($http) {
   return {
-   
+
    getAll : function () {
-          
+
      return $http.get('https://galvanize-student-apis.herokuapp.com/gdating/members?limit=10');
 
-   } 
-    
-  } 
+   }
+
+ };
 }
-  
