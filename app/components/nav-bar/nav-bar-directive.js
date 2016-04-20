ae6c7c36edd5da492f79244255d3b981580917b3
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-19 21:38:31 -0600

    Added a placeholder navbar

---
+++ app/components/nav-bar/nav-bar-directive.js
@@ -0,0 +1,14 @@
+(function(){
+  'use strict';
+
+  angular
+  .module('temperamentsApp')
+  .directive('navBar', navBar);
+
+function navBar() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/nav-bar/nav-bar.html'  
+  };
+}
+})();
